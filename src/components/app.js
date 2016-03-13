9ddf4c576554c7a162310a6523169b4906805620
Jose Vildosola <josevildosola@MacBook-Air-de-Jose.local>
2016-03-12 23:47:56 -0300

    Landing page component finished (Dumb component)

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>{this.props.children}</div>
+      <div className="app-container">{this.props.children}</div>
     );
   }
 }
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router';
+import ReactDriveIn from 'react-drive-in';
 
 const Landing = (props) => {
   return (
-    <div>
-      <ul className="main-nav">
-        <li><img className="logo" src="http://static.tecnocino.it/tcwww/fotogallery/150X94/34479/facebook-pet-society-logo.jpg" /></li>
-        <li><Link to="/">Home</Link></li>
-      </ul>
+    <div className="landing-container">
+        <div className="header">
+          <ul className="main-nav">
+            <li><img className="logo" height="94" width="250"  src="/imgs/Logo.png" /></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">Services</Link></li>
+            <li><Link to="/">Pricing</Link></li>
+          <li><Link to="/">Log In</Link></li>
+          </ul>
+        </div>
+        <div className="landing-body">
+          <div className="landing-title">
+            <span className="white-color font-landing">Because we care about them</span>
+          </div>
+          <div className="landing-subtitle">
+            <span className="white-color font-landing">We care about you</span>
+          </div>
+        </div>
     </div>
   );
 }
